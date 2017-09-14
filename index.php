1d7c2b83addab67dde0268244917d2b99a5c064b
Molly Arhammar <mollyarhammar@gmail.com>
2017-09-14 15:57:14 +0200

    make some sort of tentative structure

--- index.php
+++ index.php
@@ -1,53 +1,26 @@
 <?php
 session_start();
-$_SESSION['isLoggedIn'] = true;
-//Set session variable
 $isLoggedIn = $_SESSION ? $_SESSION["isLoggedIn"] : false;
 
 //INCLUDE THE FILES NEEDED...
-require_once('view/LoginView.php');
-require_once('view/DateTimeView.php');
-require_once('view/LayoutView.php');
+require_once('loginiew/Loginloginiew.php');
+require_once('loginiew/DateTimeloginiew.php');
+require_once('loginiew/Layoutloginiew.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 //CREATE OBJECTS OF THE VIEWS
-$v = new LoginView();
-$dtv = new DateTimeView();
-$lv = new LayoutView();
+$loggedInView = new LoggedInView();
+$dateTime = new DateTimeView();
+$layout = new LayoutView();
 
+if ($isLoggedIn) {
+    $layout->render($loggedInView, $dateTime);
+}
+else {
+    $layout->render($loggedOutView, $dateTime);
+}
 
-$lv->render($isLoggedIn, $v, $dtv);
-
-echo '<html><head><title>Test MySQL</title></head><body>';
-$host = 'localhost';
-$user = $_ENV["MYSQL_USERNAME"];
-$password = $_ENV["MYSQL_PASSWORD"];
-$cxn = mysqli_connect($host,$user,$password);
-$sql='SHOW DATABASES';
-$result = mysqli_query($cxn,$sql);
-
-if($result == false)
-{
-    echo '<h4>Error: '.mysqli_error($cxn).'</h4>';
-}
-else
-{
-if(mysqli_num_rows($result) < 1)
-{
-    echo '<p>No current databases</p>';
-}
-else
-{
-    echo '<ol>';
-while($row = mysqli_fetch_row($result))
-{
-    echo '<li>' . $row[0] . '</li>';
-}
-    echo '</ol>';
-}
-}
-    echo '</body></html>';
 ?>
--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -1,11 +1,10 @@
 <?php
+	class DateTimeView {
 
-class DateTimeView {
-
-	public function show() {
-
-		$timeString = date('l\, \t\h\e j\t\h \o\f F Y, \T\h\e \t\i\m\e \i\s H\:i\:s\.'); ;
-
-		return '<p>' . $timeString . '</p>';
+		public function show() {
+			$timeString = date('l\, \t\h\e j\t\h \o\f F Y, \T\h\e \t\i\m\e \i\s H\:i\:s\.');
+			
+			return '<p>' . $timeString . '</p>';
+		}
 	}
-}
+?>
