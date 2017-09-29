843676aecb5628203a223c2d0f894d9963a043bf
Molly Arhammar <mollyarhammar@gmail.com>
2017-09-29 12:29:29 +0200

    update error messages

--- model/Exception.php
+++ model/Exception.php
@@ -76,7 +76,7 @@ class WrongInfoInCookieException extends WrongCredentialsException
 
 class RegisterUserException extends \Exception
 {
-    public function __construct($message, $code = 0, \Exception $previous = null)
+    public function __construct($message = '', $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
