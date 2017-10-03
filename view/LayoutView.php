61204de4973969169319d837adb2424771e6d9b8
Molly Arhammar <mollyarhammar@gmail.com>
2017-10-03 14:27:05 +0200

    first commit on branch

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -18,7 +18,7 @@ class LayoutView
       <html>
         <head>
           <meta charset="utf-8">
-          <title>Login Example</title>
+          <title>Login Example With Added Functionality</title>
         </head>
         <body>
           <h1>Assignment 2</h1>
